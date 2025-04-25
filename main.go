f43c7816042b1b698012ba58ad8ed5bab297aab8
ShowravKormokar <showravkormokar354@gmail.com>
2025-04-25 23:54:12 +0600

    Write first code using GO language

---
+++ main.go
@@ -0,0 +1,7 @@
+package main //Building an executable program.  It tells Go: “This file should be compiled into a runnable binary.”
+
+import "fmt" //fmt stands for "format". It provides functions to format and print output, like Println, Printf, etc.
+
+func main() {
+	fmt.Println("Hello, World! This is the first code of GO language.")
+}
