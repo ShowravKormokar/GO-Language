4c0a68cb6a9a2a332349a190cf11f6c8824f49bc
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-01 21:09:55 +0600

    Make jwt middlware for create protected routes

---
+++ Server/httpServer_Auth_JWT_MongoDB/middleware/jwtMiddleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"httpServer_JWT_MongoDB/dto"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type contextKey string
+
+const UserContextKey contextKey = "user"
+
+func JWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
+		authHeader := rq.Header.Get("Authorization")
+
+		if authHeader == "" {
+			rw.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(rw).Encode(dto.BasicResponse{
+				Success: false,
+				Message: "Authorization header missing",
+			})
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte("my_super_secret_256key"), nil
+		})
+		if err != nil || !token.Valid {
+
+			rw.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(rw).Encode(dto.BasicResponse{
+				Success: false,
+				Message: "Invalid token",
+			})
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		ctx := context.WithValue(rq.Context(), UserContextKey, claims)
+		next.ServeHTTP(rw, rq.WithContext(ctx))
+	})
+}
