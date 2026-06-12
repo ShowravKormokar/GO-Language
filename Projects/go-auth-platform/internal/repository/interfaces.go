8575d3bb1f152d888b941c3322cf69db27e70826
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-12 12:40:39 +0600

    Create repository interface to declare method for DB query logic

---
+++ Projects/go-auth-platform/internal/repository/interfaces.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"context"
+	"go-auth-platform/internal/models"
+
+	"github.com/google/uuid"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *models.User) error
+	FindByEmail(ctx context.Context, email string) (*models.User, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*models.User, error)
+	Update(ctx context.Context, user *models.User) error
+	Delete(ctx context.Context, id uuid.UUID) error
+	List(ctx context.Context, page int, limit int, search string) ([]models.User, int64, error)
+}
+
+type RoleRepository interface {
+	FindByName(ctx context.Context, name string) (*models.Role, error)
+	FindAll(ctx context.Context) ([]models.Role, error)
+}
+
+type RefreshTokenRepository interface {
+	Create(ctx context.Context, token *models.RefreshToken) error
+	FindByHash(ctx context.Context, hash string) (*models.RefreshToken, error)
+	RevokeByUserID(ctx context.Context, userId uuid.UUID) error
+}
+
+type BlacklistRepository interface {
+	Create(ctx context.Context, token *models.BlacklistedToken) error
+	ExistsByJTI(ctx context.Context, jti string) (bool, error)
+	DeleteExpired(ctx context.Context) error
+}
