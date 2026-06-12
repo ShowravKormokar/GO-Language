5172c0f482e2bbee2fb4e75ecddacaf053ae53cd
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-12 18:41:13 +0600

    Modify to improve password error handling

--- Server/httpServer_Auth_JWT_MongoDB/utils/password.go
+++ Server/httpServer_Auth_JWT_MongoDB/utils/password.go
@@ -1,22 +1,39 @@
 package utils
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	BcryptCost = bcrypt.DefaultCost
 )
 
 // Convert password into hash from
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword(
 		[]byte(password),
-		bcrypt.DefaultCost,
+		BcryptCost,
 	)
-	return string(bytes), err
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
 
 // Check password match or not
 func CheckPassword(hash, password string) error {
-	return bcrypt.CompareHashAndPassword(
+	err := bcrypt.CompareHashAndPassword(
 		[]byte(hash),
 		[]byte(password),
 	)
+
+	if err != nil {
+		return errors.New("invalid credentials")
+	}
+
+	return nil
 }
