b83157ae236ecf3643486962e7db33649ebbacf8
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-13 16:55:40 +0600

    Make method for hashing using SHA256

---
+++ Projects/go-auth-platform/internal/utils/hash.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func SHA256Hash(input string) string {
+	hash := sha256.Sum256([]byte(input))
+
+	return hex.EncodeToString(hash[:])
+}
