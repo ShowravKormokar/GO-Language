02511047cbab316cc9c1a70aee239bc32fe9b5ea
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-13 17:25:12 +0600

    Create method to generate access token

--- Projects/go-auth-platform/internal/utils/jwt.go
+++ Projects/go-auth-platform/internal/utils/jwt.go
@@ -1,2 +1,41 @@
 package utils
 
+import (
+	"go-auth-platform/internal/config"
+	dto "go-auth-platform/internal/dto/claims"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type TokenPair struct {
+	AccessToken, RefreshToken string
+}
+
+func GenerateAccessToken(userID string, email string, role string) (string, string, error) {
+	jti := uuid.NewString()
+
+	claims := dto.JWTClaims{
+		UserID: userID,
+		Email:  email,
+		Role:   role,
+		JTI:    jti,
+
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   userID,
+			ID:        jti,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(config.AppConfig.JWTAccessTTL)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signed, err := token.SignedString([]byte(config.AppConfig.JWTSecret))
+	if err != nil {
+		return "", "", err
+	}
+
+	return signed, jti, nil
+}
