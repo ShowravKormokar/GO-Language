d65f903b4373359317ff2e3b80c1a9bc77dea454
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-19 22:07:53 +0600

    Make user get-me handler

---
+++ Projects/go-auth-platform/internal/handler/user_handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"go-auth-platform/internal/constants"
+	cmmRes "go-auth-platform/internal/dto/common"
+	"go-auth-platform/internal/service"
+	"go-auth-platform/internal/utils"
+	"net/http"
+)
+
+type UserHandler struct {
+	userService *service.AuthService
+}
+
+func NewUserHandler(userService *service.AuthService) *UserHandler {
+	return &UserHandler{
+		userService: userService,
+	}
+}
+
+// Get-Me handler
+func (u *UserHandler) Me(rw http.ResponseWriter, rq *http.Request) {
+	// Get user id from context
+	userID := rq.Context().Value(constants.ContextUserID).(string)
+
+	// Get user by id
+	user, err := u.userService.GetCurrentUser(rq.Context(), userID)
+	if err != nil {
+		http.Error(
+			rw,
+			"user not found",
+			http.StatusNotFound,
+		)
+		return
+	}
+
+	// Success response
+	utils.JSON(
+		rw,
+		http.StatusOK,
+		cmmRes.APIResponse[any]{
+			Success: true,
+			Data:    user,
+		},
+	)
+}
