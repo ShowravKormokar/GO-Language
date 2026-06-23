213dfe9464c186b928c77fd3773ef6214e2e3ba5
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-23 20:42:07 +0600

    [create] reset password service

--- Projects/go-auth-platform/internal/service/password_service.go
+++ Projects/go-auth-platform/internal/service/password_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"go-auth-platform/internal/models"
 	"go-auth-platform/internal/repository"
 	"go-auth-platform/internal/utils"
@@ -53,4 +54,57 @@ func (s *PasswordService) ForgotPassword(ctx context.Context, email string) (str
 	link := "http://localhost:8080/reset-password?token=" + token
 
 	return link, nil
+}
+
+// Reset password logic
+func (s *PasswordService) ResetPassword(ctx context.Context, token string, newPassword string) error {
+
+	// Hash the given token
+	hashToken := utils.HashResetToken(token)
+
+	// Find the token to check validaty
+	reset, err := s.resetRepo.FindValidToken(ctx, hashToken)
+	if err != nil {
+		return errors.New("Invalid or expired token")
+	}
+
+	// Check expired or not
+	if time.Now().After(reset.ExpiresAt) {
+		return errors.New("reset link expired")
+	}
+
+	// Find user
+	user, err := s.userRepo.FindByID(ctx, reset.UserID)
+	if err != nil {
+		return err
+	}
+
+	// Hash new password
+	newPassHash, err := utils.HashPassword(newPassword)
+	if err != nil {
+		return errors.New("Something went wrong")
+	}
+
+	// Set new password
+	user.PasswordHash = newPassHash
+
+	// Update password
+	err = s.userRepo.Update(ctx, user)
+	if err != nil {
+		return errors.New("password not update")
+	}
+
+	// Logout from all devices
+	err = s.refreshRepo.RevokeByUserID(ctx, user.ID)
+	if err != nil {
+		return errors.New("service not avialable at this time")
+	}
+
+	// Delete token
+	err = s.resetRepo.MarkUsed(ctx, reset.ID)
+	if err != nil {
+		return errors.New("service not avialable at this time")
+	}
+
+	return nil
 }
