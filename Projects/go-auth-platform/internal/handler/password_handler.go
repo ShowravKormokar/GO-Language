b157645a70a5da99e1d025cd1d0cfc8ca027d1c9
ShowravKormokar <showravkormokar354@gmail.com>
2026-06-23 20:59:27 +0600

    [create] reset password handler

--- Projects/go-auth-platform/internal/handler/password_handler.go
+++ Projects/go-auth-platform/internal/handler/password_handler.go
@@ -42,6 +42,7 @@ func (h *PasswordHandler) ForgotPassword(rw http.ResponseWriter, rq *http.Reques
 			http.StatusInternalServerError,
 			err,
 		)
+		return
 	}
 	// In production the reset link sent on user email
 	utils.JSON(
@@ -51,6 +52,45 @@ func (h *PasswordHandler) ForgotPassword(rw http.ResponseWriter, rq *http.Reques
 			Success: true,
 			Message: "If email exists reset link sent",
 			Data:    link, // This is only for learning purpose
+		},
+	)
+}
+
+// Reset Password handlers
+func (h *PasswordHandler) ResetPassword(rw http.ResponseWriter, rq *http.Request) {
+	var req *dtoPR.ResetPasswordRequest
+
+	if err := json.NewDecoder(rq.Body).Decode(&req); err != nil {
+		utils.JSON(
+			rw,
+			http.StatusBadRequest,
+			dto.ErrorResponse{
+				Success: false,
+				Message: "invalid request",
+			},
+		)
+		return
+	}
+
+	err := h.passwordService.ResetPassword(rq.Context(), req.Token, req.NewPassword)
+	if err != nil {
+		utils.JSON(
+			rw,
+			http.StatusBadRequest,
+			dto.ErrorResponse{
+				Success: false,
+				Message: err.Error(),
+			},
+		)
+		return
+	}
+
+	utils.JSON(
+		rw,
+		http.StatusOK,
+		dto.APIResponse[any]{
+			Success: true,
+			Message: "password reset successful, please login",
 		},
 	)
 }
